84f52543f4380b2d920563c4f471a6102c1e422f
Bob-78 <45460255+Bob-78@users.noreply.github.com>
2018-12-25 15:35:30 +0100

    updates

--- follow.py
+++ follow.py
@@ -34,3 +34,36 @@ def now(amount):
 	    
 	            continue
 	            
+def unfollow_non_friends(amount):
+
+    print("function unfollow_non_friends called")
+    my_api = api.make_api()
+
+    # make a list of followers
+    followers = my_api.followers_ids(my_user_id)
+    print("...found {} follower".format(len(followers)))
+    
+    # make a list if following
+    following = my_api.friends_ids(my_user_id)
+    print("...found {} following".format(len(following)))
+       
+    # makes a new list of users who don't follow you back.
+    non_mutuals = list(set(following) - set(followers))
+    print("...found {} non_mutuals / non_friends".format(len(non_mutuals)))
+    
+    # unfollow our non_friends
+    if len(non_mutuals) > amount:
+        for f in non_mutuals[0:amount]:
+            try:
+                # unfollows non follower.
+                my_api.destroy_friendship(f)
+                print('...Unfollowed user {}. Waiting a bit.'.format(f))
+                time.sleep(10)
+            except:
+                print("...could not unfollow user {}".format(f))
+                continue
+    else:
+        print("Too few non_friends to unfollow now. Doing nothing for the moment.")
+        pass
+
+    
